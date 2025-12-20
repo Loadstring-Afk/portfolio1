47f53bf71f7e70905db6f70847099d7fa31244f6
Loadstring-Afk <elvismateopedro@gmail.com>
2025-12-19 18:18:40 -0600

    Update index.tsx

--- src/common/components/sections/hero/index.tsx
+++ src/common/components/sections/hero/index.tsx
@@ -7,8 +7,6 @@ import { useSectionInView } from "@/common/lib/hooks";
 import { useActiveSectionContext } from "@/common/stores/active-section";
 import { smoothScrollTo } from "@/common/lib/utils";
 import { motion } from "framer-motion";
-import { Linkedin } from "lucide-react";
-import Image from "next/image";
 
 export default function Hero() {
   const { ref } = useSectionInView("home");
@@ -21,23 +19,18 @@ export default function Hero() {
         id="home"
         ref={ref}
       >
-        <div
-          className={
-            "absolute left-0 top-0 h-screen w-full dark:bg-[#0000007c]"
-          }
-        ></div>
+        <div className="absolute left-0 top-0 h-screen w-full dark:bg-[#0000007c]" />
+
         <video
-          width="480"
-          height="720"
           preload="none"
           autoPlay
-          crossOrigin="anonymous"
           muted
           loop
           className="absolute -z-10 h-screen w-screen object-cover"
         >
           <source src="/layout.mp4" />
         </video>
+
         <div className="container flex flex-col items-start justify-center tracking-wide text-black dark:text-white">
           <div className="container relative flex h-full w-full flex-col items-center">
             <div className="h-72 w-[280px] text-center text-[2rem] font-extrabold sm:w-[520px] md:w-[700px] lg:mb-5 lg:w-[920px] lg:text-[3rem]">
@@ -49,60 +42,38 @@ export default function Hero() {
                 Hey!
               </motion.span>
               <br />
-              <TextAnimation delay={1} baseText={`I'm Maksym`} />
+              <TextAnimation delay={1} baseText={`I'm Celestial`} />
             </div>
+
             <motion.div
-              className="w-92 flex flex-col items-center justify-center gap-3 px-4 text-sm font-medium md:mt-12 md:flex-row lg:text-lg"
+              className="flex flex-col items-center justify-center gap-3 px-4 text-sm font-medium md:mt-12 md:flex-row lg:text-lg"
               initial={{ opacity: 0, y: 100 }}
               animate={{ opacity: 1, y: 0 }}
-              transition={{
-                delay: 0.1,
-              }}
+              transition={{ delay: 0.1 }}
             >
               <a
-                className="group flex w-64 cursor-pointer items-center justify-center gap-2 rounded-full bg-darkBg px-7 py-3 text-white outline-none transition hover:bg-lightBeige hover:text-black hover:dark:text-black sm:w-auto"
+                className="group flex w-64 cursor-pointer items-center justify-center gap-2 rounded-full bg-darkBg px-7 py-3 text-white transition hover:bg-lightBeige hover:text-black dark:hover:text-black sm:w-auto"
                 onClick={(e) => {
                   smoothScrollTo({ e, id: "contact" });
                   setActiveSection("contact");
                   setTimeOfLastClick(Date.now());
                 }}
               >
-                <span>Contact me here</span>
+                Contact me here
               </a>
 
               <a
-                className="borderBlack group flex w-64 cursor-pointer items-center justify-center gap-2 rounded-full bg-white px-7 py-3 text-black outline-none transition hover:bg-gray-100 dark:bg-white/10 dark:text-white dark:hover:bg-white/20 sm:w-auto"
+                className="group flex w-64 cursor-pointer items-center justify-center gap-2 rounded-full bg-white px-7 py-3 text-black transition hover:bg-gray-100 dark:bg-white/10 dark:text-white dark:hover:bg-white/20 sm:w-auto"
                 href="/CV_Azimov_Maksym.pdf"
                 download
               >
-                <span>Download CV</span>
+                Download CV
               </a>
-
-              <div className="flex gap-2">
-                <a
-                  className="borderBlack flex h-[50px] w-[50px] cursor-pointer items-center justify-center gap-2 rounded-full bg-white p-2 text-black transition hover:bg-gray-100 hover:text-gray-950 dark:bg-white/10 dark:text-white/60 dark:hover:bg-white/20"
-                  href="https://www.linkedin.com/in/maksym-azimov/"
-                  target="_blank"
-                >
-                  <Linkedin />
-                </a>
-                <a
-                  className="borderBlack flex h-[50px] w-[50px] cursor-pointer items-center justify-center gap-2 rounded-full bg-white p-2 text-gray-700 transition hover:bg-gray-100 hover:text-gray-950 dark:bg-white/10 dark:text-white/60 dark:hover:bg-white/20"
-                  href="https://github.com/bbyc4kes"
-                  target="_blank"
-                >
-                  <Image
-                    width={25}
-                    height={25}
-                    src={"/svgs/github.svg"}
-                    alt="github icon"
-                  />
-                </a>
-              </div>
             </motion.div>
           </div>
         </div>
       </section>
+
       <div className="flex w-full justify-center dark:bg-darkBg">
         <SectionDivider />
       </div>
